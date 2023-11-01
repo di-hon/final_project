44ca78900a72b563396b09229e146708778107fa
DiHon <hoangortp@gmail.com>
2023-11-01 22:49:59 +0700

    add brand function

--- admin-web/src/pages/AddBrand.js
+++ admin-web/src/pages/AddBrand.js
@@ -1,13 +1,60 @@
-import React from "react";
+import { React, useEffect } from "react";
 import CustomInput from "../components/CustomInput";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { createBrands } from "../features/brand/brandSlice";
 
+let schema = Yup.object().shape({
+  name: Yup.string().required("Brand name is required"),
+});
 const AddBrand = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const newBrand = useSelector((state) => state.brand);
+
+  const { isSuccess, isError, isLoading, createdBrand } = newBrand;
+  useEffect(() => {
+    if (isSuccess && createdBrand) {
+      toast.success("Brand added successfully!");
+    }
+    if (isError) {
+      toast.error("Something went wrong");
+    }
+  }, [isSuccess, isError, isLoading]);
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      dispatch(createBrands(values));
+      formik.resetForm();
+      setTimeout(() => {
+        navigate("/admin/brand-list");
+      }, 3000);
+    },
+  });
+
   return (
     <div>
       <h3 className="mb-4 title">Add Brand</h3>
       <div>
-        <form action="">
-          <CustomInput type="text" label="Enter brand" />
+        <form action="" onSubmit={formik.handleSubmit}>
+          <CustomInput
+            type="text"
+            label="Enter brand"
+            name="name"
+            onCh={formik.handleChange("name")}
+            onBl={formik.handleBlur("name")}
+            val={formik.values.name}
+          />
+          <div className="error">
+            {formik.touched.name && formik.errors.name}
+          </div>
           <button
             className="btn btn-success border-0 rounded-3 my-5"
             type="submit"
