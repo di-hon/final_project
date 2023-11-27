ec6d365977564e1a8d91e197a865b1d55bf78dc6
DiHon <hoangortp@gmail.com>
2023-11-27 10:58:15 +0700

    change == to ===

--- frontend-admin/src/pages/Login.js
+++ frontend-admin/src/pages/Login.js
@@ -48,7 +48,7 @@ const Login = () => {
         <h3 className="text-center title">Login</h3>
         <p className="text-center">Login to your account</p>
         <div className="error text-center">
-          {message.message == "Rejected" ? "You are not admin" : ""}
+          {message.message === "Rejected" ? "You are not admin" : ""}
         </div>
         <form action="" onSubmit={formik.handleSubmit}>
           <CustomInput
